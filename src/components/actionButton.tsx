90db6e742b3fc414d7e7a9bb1f1ce85f04bf04aa
Diego Souza de Andrade <diego.souzadeandrade@gmail.com>
2025-07-24 12:09:01 +0100

    Action Button Component

--- src/components/actionButton.tsx
+++ src/components/actionButton.tsx
@@ -1,18 +1,49 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 import { useSelector, UseSelector } from "react-redux";
 import { RootState, useAppDispatch } from "../store";
 import { addGame, removeGame, updateGame } from '../store/slices/collectionSlice';
+import { ActionButtonProps } from '../types/customTypes';
 
-const ActionButton = () => {
+
+const ActionButton: React.FC<ActionButtonProps> = ({ onPress, name, size, color}) => {
     const dispatch = useAppDispatch();
     const games = useSelector((state: RootState) => state.collection.games);
 
     const handleAddToWishList = () => {
         //TODO: This was just for testing, we need to recieve an game to add
-        dispatch(addGame({id:1, name: 'Zelda', status: 'wishlist', backgroundImage: ''}));
+        //dispatch(addGame({id:1, name: 'Zelda', status: 'wishlist', backgroundImage: ''}));
     }
 
     const handleRemove = () => {
         //TODO: This was just for testing, we need to recieve an game id to remove
         dispatch(removeGame(1));
     }
+
+    return (
+        <TouchableOpacity style={styles.button} onPress={() => onPress()}>
+          <Icon style={styles.iconContainer} name={name} size={size} color={color}/>
+        </TouchableOpacity>
+    )
 };
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 50,
+    width: 65,
+    height: 65,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  iconContainer:{
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default ActionButton;
--- src/types/customTypes.ts
+++ src/types/customTypes.ts
@@ -1,9 +1,26 @@
 export type GameStatus = 'playing' | 'wishlist' | 'finished';
 
+export enum GameCategory {
+    WISHLIST = 'wishlist',
+    CURRENTLY_PLAYING = 'currentlyPlaying',
+    FINISHED = 'finished',
+}
+
 export interface Game {
     id: number;
     name: string;
     status: GameStatus;
-    backgroundImage: string;
+    background_image: string;
     hoursPlayed?: number;
+    category?: GameCategory;
+    released?: string;
+    description?: string;
+    rating?: number;
+};
+
+export type ActionButtonProps = {
+    onPress: (value?: number) => void;
+    name: string;
+    size: number;
+    color: string
 };
