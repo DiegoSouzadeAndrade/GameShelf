ffa40202e82361f62ee73e9bdf51fb034433ae03
Diego Souza de Andrade <diego.souzadeandrade@gmail.com>
2025-07-24 15:05:49 +0100

    Removing test logic from buttons component

--- src/components/actionButton.tsx
+++ src/components/actionButton.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
-import { useSelector, UseSelector } from "react-redux";
-import { RootState, useAppDispatch } from "../store";
-import { addGame, removeGame, updateGame } from '../store/slices/collectionSlice';
 import { ActionButtonProps } from '../types/customTypes';
 
 
 const ActionButton: React.FC<ActionButtonProps> = ({ onPress, name, size, color}) => {
-    const dispatch = useAppDispatch();
-    const games = useSelector((state: RootState) => state.collection.games);
-
-    const handleAddToWishList = () => {
-        //TODO: This was just for testing, we need to recieve an game to add
-        //dispatch(addGame({id:1, name: 'Zelda', status: 'wishlist', backgroundImage: ''}));
-    }
-
-    const handleRemove = () => {
-        //TODO: This was just for testing, we need to recieve an game id to remove
-        dispatch(removeGame(1));
-    }
 
     return (
         <TouchableOpacity style={styles.button} onPress={() => onPress()}>
